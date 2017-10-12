da30d3fc7f0be2d7b22cacecfac85d3c13d0a5af
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2017-10-12 07:02:09 +0530

    verify and http status exception

--- src/agent.py
+++ src/agent.py
@@ -14,19 +14,15 @@ class Dialogflow(object):
 
     def __init__(self, **kwargs):
         """
-        Create a HTTP session to a server
-        :param url: API.AI base url
-        :param client_access_token: client access token
-        :param api_version: API version
-        :param language: default locale language
-        :param timezone: Current timezone
+        Create a HTTP session to a Dialogflow server
         """
-        self.url = kwargs.get('query', 'https://api.dialogflow.com/v1/')
+        self.base_url = kwargs.get('query', 'https://api.dialogflow.com/v1')
         self.client_access_token = kwargs.get('client_access_token', '')
         self.developer_access_token = kwargs.get('developer_access_token', '')
         self.api_version = kwargs.get('api_version', '20150910')
         self.language = kwargs.get('language', 'en')
         self.timezone = kwargs.get('timezone', 'Asia/Kolkata')
+        verify = kwargs.get('verify', False)
 
         # Allow different speech recognition methods to be selected
         # See https://pypi.python.org/pypi/SpeechRecognition/
@@ -38,6 +34,23 @@ class Dialogflow(object):
             'Authorization': 'Bearer ' + self.client_access_token,
         }
 
+        if isinstance(verify, bool):
+            self.session.verify = verify
+
         self.session.headers.update(self.headers)
         self.session.mount('http://', requests.adapters.HTTPAdapter())
         self.session.mount('https://', requests.adapters.HTTPAdapter())
+
+        self.session.url = self.base_url
+
+    class HTTPStatusException(Exception):
+        """
+        Exception raised when unexpected non-success HTTP
+        status codes are returned in a response.
+        """
+
+        def __init__(self, value):
+            self.value = value
+
+        def __str__(self):
+            return repr(self.value)
