ad79bda596be6b11d08e09dc36c1343801af3ad2
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2017-10-12 07:02:39 +0530

    partial entity implementation

--- src/entities.py
+++ src/entities.py
@@ -3,25 +3,62 @@ from .agent import Dialogflow
 
 
 class Entities(Dialogflow):
+    def __init__(self):
+        self.entity_url = self.base_url + '/entities'
+        self.headers = {
+            'Authorization': 'Bearer ' + self.developer_access_token,
+        }
+        self.session.headers.update(self.headers)
+
     def get_entities(self):
         """
         Retrieves a list of all entities for the agent.
         :return: JSON response
         """
-        pass
+        params = (
+            ('v', self.api_version),
+            ('lang', self.language),
+            ('sessionId', self.session_id),
+            ('timezone', self.timezone),
+        )
 
-    def get_entitiy(self, id):
+        return self.session.get(url=self.entity_url, params=params).json()
+
+    def get_entitiy(self, entity_id):
         """
         Retrieves the specified entity
         :param id: Entity ID
         :return: JSON response
         """
-        pass
+        params = (
+            ('v', self.api_version),
+            ('lang', self.language),
+            ('sessionId', self.session_id),
+            ('timezone', self.timezone),
+        )
+        entity_url = self.entity_url + "/{}".format(entity_id)
+        return self.session.get(url=entity_url, params=params).json()
 
     def create_entities(self, data):
         """
         create a new entity
-        :param data: A valid JSON data
+        :param data: A valid JSON data, example data
+        {
+            "name": "Appliances",
+            "entries": [{
+                "value": "Coffee Maker",
+                "synonyms": ["coffee maker", "coffee machine", "coffee"]
+            }, {
+                "value": "Thermostat",
+                "synonyms": ["Thermostat", "heat", "air conditioning"]
+            }, {
+                "value": "Lights",
+                "synonyms": ["lights", "light", "lamps"]
+            }, {
+                "value": "Garage door",
+                "synonyms": ["garage door", "garage"]
+            }]
+        }
         :return: JSON response
         """
         pass
