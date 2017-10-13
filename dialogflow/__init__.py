a70e067c54d9b9345f34f37a9111f2dea9d620ae
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2017-10-14 01:46:07 +0530

    bump minor version

--- dialogflow/__init__.py
+++ dialogflow/__init__.py
@@ -4,7 +4,7 @@ API Ai python requests package
 
 from .dialogflow import Dialogflow
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Mallikarjunarao Kosuri'
 __email__ = 'malli.kv2@gmail.com'
 
