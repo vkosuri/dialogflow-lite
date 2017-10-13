4f8170a3931c64d834e35fb333bf6432053f34fe
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2017-10-13 08:20:10 +0530

    voic is now part of query

--- src/query.py
+++ src/query.py
@@ -2,10 +2,14 @@
 from .agent import Dialogflow
 from .status import HTTP_200_SUCCESS, HTTP_200_DEPRECATED
 
+import speech_recognition
+import subprocess
+
 
 class Query(Dialogflow):
     query_response = {}
     previous_query_response = {}
+    query_url = None
 
     def __init__(self, **kwargs):
         super(Query, self).__init__(**kwargs)
@@ -24,6 +28,7 @@ class Query(Dialogflow):
         :param text: Input text
         :return: response message
         """
+
         params = (
             ('v', self.api_version),
             ('query', text),
@@ -36,9 +41,10 @@ class Query(Dialogflow):
             self.previous_query_response = self.query_response
 
         self.query_response = result = self.session.get(url=self.query_url, params=params).json()
+
         return result
 
-    def get_response_message(self, text):
+    def text_request(self, text):
         responses = []
         result = self.query(text)
         if result['status']['code'] == HTTP_200_SUCCESS:
@@ -50,6 +56,43 @@ class Query(Dialogflow):
             )
 
         return responses
+
+    def voice_request(self):
+        # Start jack control
+        self.attempt_jack_control_start()
+
+        recognizer = speech_recognition.Recognizer()
+        with speech_recognition.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
+
+        recognizer_function = getattr(recognizer, self.recognizer_function)
+
+        try:
+            result = recognizer_function(audio)
+            return result
+        except speech_recognition.UnknownValueError:
+            return 'I am sorry, I could not understand that.'
+        except speech_recognition.RequestError as e:
+            m = 'My speech recognition service has failed. {0}'
+            return m.format(e)
+
+    def attempt_jack_control_start(self):
+        """
+        Jack is a program that can be used to get audio
+        input from your system. This command will try
+        to start it when your program runs.
+        """
+        import warnings
+
+        try:
+            subprocess.call(['jack_control', 'start'])
+        except self.DialogflowQueryException:
+            # Note: jack_control is not a valid command in Windows
+            warnings.warn(
+                'Unable to start jack control.',
+                RuntimeWarning
+            )
 
     def _validate_status_code(self, response):
         code = response['status']['code']
