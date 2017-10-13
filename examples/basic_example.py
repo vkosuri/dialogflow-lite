2c9d244ba172c03198f88a230013f3247bc8cd02
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2017-10-14 01:08:35 +0530

    example update

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -4,8 +4,8 @@ from dialogflow import Dialogflow
 
 # demo agent access token: e5dc21cab6df451c866bf5efacb40178
 client_access_token = 'e5dc21cab6df451c866bf5efacb40178'
-dialogflow = Dialogflow(client_access_token)
-response = dialogflow.query('how are you')
+dialogflow = Dialogflow(client_access_token=client_access_token)
+response = dialogflow.text_request('how are you')
 print(response)
-response = dialogflow.query('howz the weather today')
+response = dialogflow.text_request('howz the weather today')
 print(response)
