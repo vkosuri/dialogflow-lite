d455ea5edbf3fd9ed4d49dd3c5d630b65d9a7d9c
Mallikarjunarao Kosuri <malli.kv2@gmail.com>
2019-03-19 09:10:31 +0530

    version bump

--- dialogflow_lite/__init__.py
+++ dialogflow_lite/__init__.py
@@ -6,7 +6,7 @@ import sys
 if 'install' not in sys.argv and 'egg_info' not in sys.argv:
     from .dialogflow import Dialogflow
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 __author__ = 'Mallikarjunarao Kosuri'
 __email__ = 'malli.kv2@gmail.com'
 
